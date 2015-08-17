c15f6e7ecb66d2488f1fd2317a6e1030543d0622
Kyle Barbary <kylebarbary@gmail.com>
2015-08-17 15:05:01 -0700

    change version numbers in .travis.yml

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ classifiers = [
     "Development Status :: 3 - Alpha Development Status",
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: BSD License",
+    "License :: OSI Approved :: MIT License",
+    "Topic :: Scientific/Engineering",
     "Topic :: Scientific/Engineering :: Astronomy",
     "Intended Audience :: Science/Research"]
 
@@ -17,7 +18,7 @@ setup(name="nestle",
       version="0.1.0.dev",
       description=description,
       long_description=description,
-      license = "BSD",
+      license = "MIT",
       classifiers=classifiers,
       py_modules=["nestle"],
       url="https://github.com/kbarbary/nestle",

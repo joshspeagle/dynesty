48a29f96cdaedf99304d4220174e96321d2445f1
Kyle Barbary <kylebarbary@gmail.com>
2015-08-17 15:27:09 -0700

    ugh, travis and conda already.

--- setup.py
+++ setup.py
@@ -3,24 +3,20 @@ import os
 from glob import glob
 from distutils.core import setup
 
-description = "nested sampling algorithms for computing Bayesian evidence"
-
-classifiers = [
-    "Development Status :: 3 - Alpha Development Status",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
-    "Topic :: Scientific/Engineering",
-    "Topic :: Scientific/Engineering :: Astronomy",
-    "Intended Audience :: Science/Research"]
+url = "https://github.com/kbarbary/nestle"
 
 setup(name="nestle", 
       version="0.1.0.dev",
-      description=description,
-      long_description=description,
-      license = "MIT",
-      classifiers=classifiers,
+      description="Nested sampling algorithms for evaluating Bayesian evidence",
+      long_description=url,
+      classifiers = ["Development Status :: 2 - Pre-Alpha",
+                     "Programming Language :: Python :: 2",
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Topic :: Scientific/Engineering",
+                     "Topic :: Scientific/Engineering :: Astronomy",
+                     "Intended Audience :: Science/Research"],
       py_modules=["nestle"],
-      url="https://github.com/kbarbary/nestle",
+      url=url,
       author="Kyle Barbary",
       author_email="kylebarbary@gmail.com")

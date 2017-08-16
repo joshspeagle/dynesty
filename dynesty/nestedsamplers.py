40c9bfc35c5265a6f02b212e4aff3e5e0e3c4262
Josh Speagle <joshspeagle@gmail.com>
2017-08-16 19:39:29 -0400

    bounding bugfix^2
    Fixed a small bug I introduced when I changed the `pointvol` computation
    within the `MultiEllipsoidSampler`.

--- dynesty/nestedsamplers.py
+++ dynesty/nestedsamplers.py
@@ -534,7 +534,10 @@ class MultiEllipsoidSampler(Sampler):
 
         # Automatically trigger an update if we're not in any ellipsoid.
         if nidx == 0:
-            expected_vol = math.exp(self.saved_logvol[-1] - self.dlv)
+            try:
+                expected_vol = math.exp(self.saved_logvol[-1] - self.dlv)
+            except:
+                expected_vol = math.exp(-self.dlv)
             pointvol = expected_vol / self.nlive
             prop = self.update(pointvol)
             if self.save_proposals:

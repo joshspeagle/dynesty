ed9f3dc301d74350553c8d43f6dc45cee671014d
Johannes Buchner <buchner.johannes@gmx.at>
2019-02-05 15:31:16 -0500

    added intermediate length, and better compatibility

--- dynesty/results.py
+++ dynesty/results.py
@@ -113,20 +113,29 @@ def print_fn(results, niter, ncall, add_live_it=None,
                                                              loglstar,
                                                              logl_max))
     long_str.append("logz: {:6.3f} +/- {:6.3f}".format(logz, logzerr))
-    short_str.append("logz: {:6.1f} +/- {:6.1f}".format(logz, logzerr))
+    short_str.append("logz: {:6.1f}+/-{:.1f}".format(logz, logzerr))
+    mid_str = list(short_str)
     if dlogz is not None:
         long_str.append("dlogz: {:6.3f} > {:6.3f}".format(delta_logz, dlogz))
+        mid_str.append("dlogz: {:6.1f}>{:6.1f}".format(delta_logz, dlogz))
     else:
         long_str.append("stop: {:6.3f}".format(stop_val))
+        mid_str.append("stop: {:6.3f}".format(stop_val))
     #long_str.append("            ")  # clear previous output
     #short_str.append("  ".format(logz, logzerr))
 
     # Printing.
     long_str = ' | '.join(long_str)
+    mid_str = ' | '.join(mid_str)
     short_str = '|'.join(short_str)
-    columns, rows = shutil.get_terminal_size(fallback=(80, 25))
+    if sys.stderr.isatty() and hasattr(shutil, 'get_terminal_size'):
+        columns, rows = shutil.get_terminal_size(fallback=(80, 25))
+    else:
+        columns, rows = 1000, 25
     if columns > len(long_str):
         sys.stderr.write("\r" + long_str + ' '*(columns-len(long_str)-2) + '\r')
+    elif columns > len(mid_str):
+        sys.stderr.write("\r" + mid_str + ' '*(columns-len(mid_str)-2) + '\r')
     else:
         sys.stderr.write("\r" + short_str + ' '*(columns-len(short_str)-2) + '\r')
     sys.stderr.flush()

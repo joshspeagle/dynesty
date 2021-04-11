651ec05e180ad6a2e79cfd1e8a68a4004cd4857a
Sergey E. Koposov <skoposov@ed.ac.uk>
2021-04-11 17:52:40 +0100

    make sure that the eggbox problem is tested with different boundary algos

--- tests/test_egg.py
+++ tests/test_egg.py
@@ -33,23 +33,24 @@ def prior_transform_egg(x):
     return x * 10 * np.pi
 
 
-def test_ellipsoids():
-    # stress test ellipsoid decompositions
+def test_bounds():
+    # stress test various boundaries
     ndim = 2
-    sampler = dynesty.NestedSampler(loglike_egg,
-                                    prior_transform_egg,
-                                    ndim,
-                                    nlive=nlive,
-                                    bound='multi',
-                                    sample='unif')
-    sampler.run_nested(dlogz=0.01, print_progress=printing)
-    logz_truth = 235.856
-    assert (abs(logz_truth - sampler.results.logz[-1]) <
-            5. * sampler.results.logzerr[-1])
+    for bound in ['multi', 'balls', 'cubes']:
+        sampler = dynesty.NestedSampler(loglike_egg,
+                                        prior_transform_egg,
+                                        ndim,
+                                        nlive=nlive,
+                                        bound=bound,
+                                        sample='unif')
+        sampler.run_nested(dlogz=0.01, print_progress=printing)
+        logz_truth = 235.856
+        assert (abs(logz_truth - sampler.results.logz[-1]) <
+                5. * sampler.results.logzerr[-1])
 
 
 def test_ellipsoids_bootstrap():
-    # stress test ellipsoid decompositions
+    # stress test ellipsoid decompositions with bootstrap
     ndim = 2
     sampler = dynesty.NestedSampler(loglike_egg,
                                     prior_transform_egg,

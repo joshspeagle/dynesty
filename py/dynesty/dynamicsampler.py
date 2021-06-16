fix the maths to avoid overflow

py/dynesty/dynamicsampler.py
@@ -1332,10 +1332,11 @@ class DynamicSampler(object):
             logdvol, dlv = logdvols[i], dlvs[i]
             logwt = np.logaddexp(loglstar_new, loglstar) + logdvol
             logz_new = np.logaddexp(logz, logwt)
-            lzterm = (math.exp(loglstar - logz_new) * loglstar +
-                      math.exp(loglstar_new - logz_new) * loglstar_new)
-            h_new = (math.exp(logdvol) * lzterm + math.exp(logz - logz_new) *
-                     (h + logz) - logz_new)
+            lzterm = (
+                math.exp(loglstar - logz_new + logdvol) * loglstar +
+                math.exp(loglstar_new - logz_new + logdvol) * loglstar_new)
+            h_new = (lzterm + math.exp(logz - logz_new) * (h + logz) -
+                     logz_new)
             dh = h_new - h
             h = h_new
             logz = logz_new

py/dynesty/sampler.py
@@ -503,10 +503,11 @@ class Sampler(object):
             # Compute relative contribution to results.
             logwt = np.logaddexp(loglstar_new, loglstar) + logdvol  # weight
             logz_new = np.logaddexp(logz, logwt)  # ln(evidence)
-            lzterm = (math.exp(loglstar - logz_new) * loglstar +
-                      math.exp(loglstar_new - logz_new) * loglstar_new)
-            h_new = (math.exp(logdvol) * lzterm + math.exp(logz - logz_new) *
-                     (h + logz) - logz_new)  # information
+            lzterm = (
+                math.exp(loglstar - logz_new + logdvol) * loglstar +
+                math.exp(loglstar_new - logz_new + logdvol) * loglstar_new)
+            h_new = (lzterm + math.exp(logz - logz_new) * (h + logz) - logz_new
+                     )  # information
             dh = h_new - h
             h = h_new
             logz = logz_new
@@ -803,10 +804,11 @@ class Sampler(object):
 
             # Update evidence `logz` and information `h`.
             logz_new = np.logaddexp(logz, logwt)
-            lzterm = (math.exp(loglstar - logz_new) * loglstar +
-                      math.exp(loglstar_new - logz_new) * loglstar_new)
-            h_new = (math.exp(logdvol) * lzterm + math.exp(logz - logz_new) *
-                     (h + logz) - logz_new)
+            lzterm = (
+                math.exp(loglstar - logz_new + logdvol) * loglstar +
+                math.exp(loglstar_new - logz_new + logdvol) * loglstar_new)
+            h_new = (lzterm + math.exp(logz - logz_new) * (h + logz) -
+                     logz_new)
             dh = h_new - h
             h = h_new
             logz = logz_new

py/dynesty/utils.py
@@ -711,9 +711,9 @@ def resample_run(res, rstate=None, return_idx=False):
         logdvol, dlv = logdvols[i], dlvs[i]
         logwt = np.logaddexp(loglstar_new, loglstar) + logdvol
         logz_new = np.logaddexp(logz, logwt)
-        lzterm = (math.exp(loglstar - logz_new) * loglstar +
-                  math.exp(loglstar_new - logz_new) * loglstar_new)
-        h_new = (math.exp(logdvol) * lzterm + math.exp(logz - logz_new) *
+        lzterm = (math.exp(loglstar - logz_new + logdvol) * loglstar +
+                  math.exp(loglstar_new - logz_new + logdvol) * loglstar_new)
+        h_new = (lzterm + math.exp(logz - logz_new) *
                  (h + logz) - logz_new)
         dh = h_new - h
         h = h_new
@@ -851,12 +851,9 @@ def reweight_run(res, logp_new, logp_old=None):
         logdvol, dlv = logdvols[i], dlvs[i]
         logwt = np.logaddexp(loglstar_new, loglstar) + logdvol + logrwt[i]
         logz_new = np.logaddexp(logz, logwt)
-        try:
-            lzterm = (math.exp(loglstar - logz_new) * loglstar +
-                      math.exp(loglstar_new - logz_new) * loglstar_new)
-        except:
-            lzterm = 0.
-        h_new = (math.exp(logdvol) * lzterm + math.exp(logz - logz_new) *
+        lzterm = (math.exp(loglstar - logz_new + logdvol ) * loglstar +
+                      math.exp(loglstar_new - logz_new + logdvol) * loglstar_new)
+        h_new = lzterm + math.exp(logz - logz_new) *
                  (h + logz) - logz_new)
         dh = h_new - h
         h = h_new
@@ -963,9 +960,9 @@ def unravel_run(res, save_proposals=True, print_progress=True):
             logdvol, dlv = logdvols[i], dlvs[i]
             logwt = np.logaddexp(loglstar_new, loglstar) + logdvol
             logz_new = np.logaddexp(logz, logwt)
-            lzterm = (math.exp(loglstar - logz_new) * loglstar +
-                      math.exp(loglstar_new - logz_new) * loglstar_new)
-            h_new = (math.exp(logdvol) * lzterm + math.exp(logz - logz_new) *
+            lzterm = (math.exp(loglstar - logz_new + logdvol) * loglstar +
+                      math.exp(loglstar_new - logz_new + logdvol) * loglstar_new)
+            h_new = (lzterm + math.exp(logz - logz_new) *
                      (h + logz) - logz_new)
             dh = h_new - h
             h = h_new
@@ -1540,9 +1537,9 @@ def _merge_two(res1, res2, compute_aux=False):
             logdvol, dlv = logdvols[i], dlvs[i]
             logwt = np.logaddexp(loglstar_new, loglstar) + logdvol
             logz_new = np.logaddexp(logz, logwt)
-            lzterm = (math.exp(loglstar - logz_new) * loglstar +
-                      math.exp(loglstar_new - logz_new) * loglstar_new)
-            h_new = (math.exp(logdvol) * lzterm + math.exp(logz - logz_new) *
+            lzterm = (math.exp(loglstar - logz_new + logdvol) * loglstar +
+                      math.exp(loglstar_new - logz_new + logdvol) * loglstar_new)
+            h_new = (lzterm + math.exp(logz - logz_new) *
                      (h + logz) - logz_new)
             dh = h_new - h
             h = h_new
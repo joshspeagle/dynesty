6a5c44347cbce0eb52dc0457f778ebfb8faf61aa
Sergey Koposov <skoposov@ed.ac.uk>
2021-08-23 22:31:50 +0100

    decrease pvalues of ellipsoid test to avoid spurious failures. Also parametrize the test and test in 2 and 10 dimensions

--- tests/test_ellipsoid.py
+++ tests/test_ellipsoid.py
@@ -1,26 +1,42 @@
-import dynesty.bounding as db
+import itertools
 import numpy as np
 import scipy.stats
+import pytest
+import dynesty.bounding as db
 from utils import get_rstate
 
+FAILURE_THRESHOLD = 1 / 1000.
+# I want this test to fail once in 1000 iterations
+# because there are 16 of these tests and the probability is twosided
+PVAL = FAILURE_THRESHOLD / 16 / 2.
 
-def test_sample():
+
+@pytest.mark.parametrize("withq,ndim",
+                         list(itertools.product([False, True], [2, 10])))
+def test_sample(withq, ndim):
     # test sampling of two overlapping ellipsoids that samples are uniform
     # within
     rad = 1
     shift = 0.75
-    ndim = 10
     cen1 = np.zeros(ndim)
     cen2 = np.zeros(ndim)
     cen2[0] = shift
-    sig = np.eye(10) * rad**2
+    sig = np.eye(ndim) * rad**2
     ells = [db.Ellipsoid(cen1, sig), db.Ellipsoid(cen2, sig)]
     mu = db.MultiEllipsoid(ells)
     R = []
     nsim = 100000
     rstate = get_rstate()
-    for i in range(nsim):
-        R.append(mu.sample(rstate=rstate)[0])
+    if withq:
+        for i in range(nsim):
+            while True:
+                x, _, q = mu.sample(return_q=True, rstate=rstate)
+                if rstate.uniform() < 1. / q:
+                    R.append(x)
+                    break
+    else:
+        for i in range(nsim):
+            R.append(mu.sample(rstate=rstate)[0])
     R = np.array(R)
     assert (all([mu.contains(_) for _ in R]))
 
@@ -34,46 +50,6 @@ def test_sample():
         xdist1 = xdist1[xdist1 < 1]
         pval = scipy.stats.kstest(xdist1,
                                   scipy.stats.uniform(loc=0.0, scale=1).cdf)[1]
-        assert ((pval > 0.003) & (pval < 0.997))
+        assert ((pval > PVAL) & (pval < (1 - PVAL)))
     nhalf = (R[:, 0] > shift / 2.).sum()
-    print(nhalf, nsim)
-    assert (np.abs(nhalf - 0.5 * nsim) < 5 * np.sqrt(0.5 * nsim))
-
-
-def test_sample_q():
-    # test sampling of two overlapping ellipsoids that samples are uniform
-    # within
-    rad = 1
-    shift = 0.75
-    ndim = 10
-    cen1 = np.zeros(ndim)
-    cen2 = np.zeros(ndim)
-    cen2[0] = shift
-    sig = np.eye(10) * rad**2
-    ells = [db.Ellipsoid(cen1, sig), db.Ellipsoid(cen2, sig)]
-    mu = db.MultiEllipsoid(ells)
-    R = []
-    nsim = 100000
-    rstate = get_rstate()
-    for i in range(nsim):
-        while True:
-            x, _, q = mu.sample(return_q=True, rstate=rstate)
-            if rstate.uniform() < 1. / q:
-                R.append(x)
-                break
-    R = np.array(R)
-    assert (all([mu.contains(_) for _ in R]))
-    # here I'm checking that all the points are uniformly distributed
-    # within each ellipsoid
-    for curc in [cen1, cen2]:
-        dist1 = (np.sqrt(np.sum((R - curc)**2, axis=1)) / rad)
-        # radius from 0 to 1
-        xdist1 = dist1**ndim
-        # should be uniformly distributed from 0 to 1
-        xdist1 = xdist1[xdist1 < 1]
-        pval = scipy.stats.kstest(xdist1,
-                                  scipy.stats.uniform(loc=0.0, scale=1).cdf)[1]
-        assert ((pval > 0.003) & (pval < 0.997))
-    nhalf = (R[:, 0] > shift / 2.).sum()
-    print(nhalf, nsim)
     assert (np.abs(nhalf - 0.5 * nsim) < 5 * np.sqrt(0.5 * nsim))

4c80a8f13e29334ff02ffc100bdab650e4e8b32b
Sergey E. Koposov <skoposov@ed.ac.uk>
2021-12-14 16:37:01 +0000

    avoid running tests on nbconverted notebooks

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,12 +1,17 @@
 import pytest
 import glob
 import os
+import re
 
 
 @pytest.mark.slow
 @pytest.mark.parametrize("nb", glob.glob('demos/*nb'))
 def test_notebooks(nb):
     """ Test that notebooks run fine """
+    if re.match('.*nbconvert.*', nb) is not None:
+        # nbconvert leaves files like nbconvert.ipynb
+        # we don't want to run on those
+        return
     cmd = f'jupyter nbconvert --to notebook --execute "{nb}"'
     stat = os.system(cmd)
     assert stat == 0

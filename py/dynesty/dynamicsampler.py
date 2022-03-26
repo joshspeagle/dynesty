joshspeagle/dynesty

commit 2cfea29acb1f14c2ef687c4509efe85353e2cf2f

remove unused variable

py/dynesty/dynamicsampler.py
@@ -300,8 +300,7 @@ def stopping_function(results,
         seeds = get_seed_sequence(rstate, n_mc)
         args = zip(rlist, error_list, approx_list, seeds)
         outputs = list(M(_kld_error, args))
-        kld_arr, lnz_arr = np.array([(kld[-1], res.logz[-1])
-                                     for kld, res in outputs]).T
+        lnz_arr = np.array([res.logz[-1] for res in outputs])
         # Evidence stopping value.
         lnz_std = np.std(lnz_arr)
     else:

commit 1277dbfbae2d9257522e7eebf3e7409eaf9a8abb

fix the bug introduced in 2cfea29acb1f14c2ef687c4509efe85353e2cf2f

py/dynesty/dynamicsampler.py
@@ -300,7 +300,7 @@ def stopping_function(results,
         seeds = get_seed_sequence(rstate, n_mc)
         args = zip(rlist, error_list, approx_list, seeds)
         outputs = list(M(_kld_error, args))
-        lnz_arr = np.array([res.logz[-1] for res in outputs])
+        lnz_arr = np.array([res[1].logz[-1] for res in outputs])
         # Evidence stopping value.
         lnz_std = np.std(lnz_arr)
     else:
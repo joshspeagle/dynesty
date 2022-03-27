16ef15a0e584125325c5eaf5d62405eccf2643cf
Sergey E. Koposov <skoposov@ed.ac.uk>
2022-03-27 14:45:08 +0100

    cleanup converted ipynb

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -5,13 +5,29 @@ import re
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("nb", glob.glob('demos/*nb'))
+@pytest.mark.parametrize("nb", glob.glob('demos/*.ipynb'))
 def test_notebooks(nb):
     """ Test that notebooks run fine """
     if re.match('.*nbconvert.*', nb) is not None:
         # nbconvert leaves files like nbconvert.ipynb
         # we don't want to run on those
         return
-    cmd = f'jupyter nbconvert --to notebook --execute "{nb}"'
-    stat = os.system(cmd)
-    assert stat == 0
+
+    # Here I'm trying to determine if the converted nb exists
+    # if it does not I'll try to delete it after we are done
+
+    postfl = 6
+    assert nb[-postfl:] == '.ipynb'
+    fname_converted = nb[:-postfl] + '.nbconvert.ipynb'
+    if not os.path.exists(fname_converted):
+        delete = True
+    else:
+        delete = False
+
+    try:
+        cmd = f'jupyter nbconvert --to notebook --execute "{nb}"'
+        stat = os.system(cmd)
+        assert stat == 0
+    finally:
+        if delete:
+            os.unlink(fname_converted)

577d2b2297a137fd300b5e9e4fd5d33afec0eefd
Sergey E. Koposov <skoposov@ed.ac.uk>
2022-03-29 15:16:14 +0100

    add changelog to setup.py install remove py2 reference from setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
 import re
-import glob
 try:
     from setuptools import setup
-    setup
 except ImportError:
     from distutils.core import setup
-    setup
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-init_string = open(os.path.join(dir_path, 'py', 'dynesty', '__init__.py')).read()
+init_string = open(os.path.join(dir_path, 'py', 'dynesty',
+                                '__init__.py')).read()
 VERS = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VERS, init_string, re.M)
 __version__ = mo.group(1)
@@ -28,30 +25,30 @@ try:
 except ImportError:
     long_description = open('README.md').read()
 
-    
 setup(
     name="dynesty",
     url="https://github.com/joshspeagle/dynesty",
     version=__version__,
     author="Joshua S Speagle",
-    author_email="jspeagle@cfa.harvard.edu",
+    author_email="j.speagle@utoronto.ca",
     packages=["dynesty"],
     license="MIT",
     description=("A dynamic nested sampling package for computing Bayesian "
                  "posteriors and evidences."),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_data={"": ["README.md", "LICENSE", "AUTHORS.md"]},
+    package_data={"": ["README.md", "LICENSE", "AUTHORS.md", 'CHANGELOG.md']},
     package_dir={'': 'py/'},
     include_package_data=True,
-    keywords=["nested sampling", "dynamic", "monte carlo", "bayesian",
-              "inference", "modeling"],
-    classifiers=["Development Status :: 5 - Production/Stable",
-                 "License :: OSI Approved :: MIT License",
-                 "Natural Language :: English",
-                 "Programming Language :: Python :: 2.7",
-                 "Programming Language :: Python :: 3.6",
-                 "Operating System :: OS Independent",
-                 "Topic :: Scientific/Engineering",
-                 "Intended Audience :: Science/Research"]
-)
+    keywords=[
+        "nested sampling", "dynamic", "monte carlo", "bayesian", "inference",
+        "modeling"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English", "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research"
+    ])

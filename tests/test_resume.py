538a76ca5a8939e1c6597f40b9d0922991a8b179
Sergey E. Koposov <skoposov@ed.ac.uk>
2022-08-22 21:23:04 +0100

    fix testing

--- tests/test_resume.py
+++ tests/test_resume.py
@@ -55,7 +55,6 @@ def fit_resume(fname, dynamic, result0):
         dns = dynesty.NestedSampler.restore(fname)
     print('resuming')
     dns.run_nested(resume=True)
-    print(dns.results.logz[-1], result0)
     assert np.abs(dns.results.logz[-1] - result0) < 1e-3
 
 
@@ -67,39 +66,38 @@ class cache:
 
 
 def getlogz():
+    # compute the time of static/dynamic runs as well
+    # logz value
     if cache.dt0 is None:
         t0 = time.time()
         result0 = fit(None, False).results.logz[-1]
         t1 = time.time()
         result1 = fit(None, True).results.logz[-1]
         t2 = time.time()
-        cache.dt0, cache.dt1, cache.res0, cache.res1 = t1 - t0, t2 - t1, result0, result1
+        (cache.dt0, cache.dt1, cache.res0, cache.res1) = (t1 - t0, t2 - t1,
+                                                          result0, result1)
     return cache.dt0, cache.dt1, cache.res0, cache.res1
 
 
-@pytest.mark.parametrize(
-    "dynamic,delay",
-    itertools.product([False, True], [.05, .5, .75, .95]
-                      #[1, 5, 10, 15]
-                      ))
-@pytest.mark.xdist_group(name="group1")
+@pytest.mark.parametrize("dynamic,delay",
+                         itertools.product([False, True], [.05, .5, .75, .95]))
+@pytest.mark.xdist_group(name="resume_group")
 def test_resume(dynamic, delay):
     pid = os.getpid()
     fname = 'xx%d.pkl' % (pid)
-    # result0 = fit(None, dynamic).results
-    dt0, dt1, res_static, res_dynamic = getlogz()
+    dt_static, dt_dynamic, res_static, res_dynamic = getlogz()
     pp = mp.Process(target=fit, args=(fname, dynamic))
     pp.start()
     fit_pid = pp.pid
     if dynamic:
-        curdt = dt0
+        curdt = dt_dynamic
     else:
-        curdt = dt1
+        curdt = dt_static
     pp = start_interrupter(fit_pid, delay * curdt)
     time.sleep(delay + 1)
     if dynamic:
-        res0 = res_dynamic
+        curres = res_dynamic
     else:
-        res0 = res_static
-    fit_resume(fname, dynamic, res0)
+        curres = res_static
+    fit_resume(fname, dynamic, curres)
     os.unlink(fname)

cfdc0210ffa94f2a773c312c76f5566d6049c109
Sergey E. Koposov <skoposov@ed.ac.uk>
2022-11-26 21:59:42 +0000

    add a test that exercises the plateau behaviour

---
+++ tests/test_plateau.py
@@ -0,0 +1,54 @@
+import numpy as np
+import dynesty
+import scipy.special
+from utils import get_rstate, get_printing
+import pytest
+
+nlive = 1000
+
+S = 3
+R = 1
+
+ndim = 2
+A0 = 1
+A1 = 10
+
+printing = get_printing()
+
+
+# likelihood that has value A1 inside a sphere with the radius R
+# and outside it has velue A0
+def loglike_inf(x):
+    r = np.sqrt(np.sum(x**2))
+    if r < R:
+        ret = np.log(A1)  # - 1e-6 * r
+    else:
+        ret = np.log(A0)  # - 1e-6 * r
+    # print(ret, r)
+    return ret
+
+
+# true value of the integral
+LOGZ_TRUE = np.log(A0 + np.pi**(ndim / 2.) /
+                   scipy.special.gamma(ndim / 2. + 1) * R**ndim * (A1 - A0) /
+                   ((2 * S)**ndim))
+
+
+def prior_transform(x):
+    return (2 * x - 1) * S
+
+
+@pytest.mark.parametrize('sample', ['unif', 'rwalk', 'rslice'])
+def test_static(sample):
+    rstate = get_rstate()
+    sampler = dynesty.NestedSampler(loglike_inf,
+                                    prior_transform,
+                                    ndim,
+                                    nlive=nlive,
+                                    rstate=rstate,
+                                    bound='none',
+                                    sample=sample)
+    sampler.run_nested(print_progress=printing)
+    res = sampler.results
+    THRESH = 5
+    assert np.abs(res.logz[-1] - LOGZ_TRUE) < THRESH * res.logzerr[-1]

fix an old bug. When the ellipsoid is being initialized as a bound, it's centred at zero and has unit variance, while it should be centred at 0.5 and have cov of ndim/4

py/dynesty/nestedsamplers.py
@@ -495,7 +495,11 @@ class SingleEllipsoidSampler(SuperSampler):
                          logvol_init=logvol_init,
                          kwargs=kwargs or {})
 
-        self.ell = Ellipsoid(np.zeros(self.ncdim), np.identity(self.ncdim))
+        self.ell = Ellipsoid(
+            np.zeros(self.ncdim) + .5,
+            np.identity(self.ncdim) * self.ncdim / 4)
+        # this is ellipsoid in the center of the cube that contains
+        # the whole cube
         self.bounding = 'single'
 
     def update(self, subset=slice(None)):
@@ -656,8 +660,11 @@ class MultiEllipsoidSampler(SuperSampler):
                          logvol_init=logvol_init,
                          kwargs=kwargs or {})
 
-        self.mell = MultiEllipsoid(ctrs=[np.zeros(self.ncdim)],
-                                   covs=[np.identity(self.ncdim)])
+        self.mell = MultiEllipsoid(
+            ctrs=[np.zeros(self.ncdim) + .5],
+            covs=[np.identity(self.ncdim) * self.ncdim / 4])
+        # this is ellipsoid in the center of the cube that contains
+        # the whole cube
         self.bounding = 'multi'
 
     def update(self, subset=slice(None)):
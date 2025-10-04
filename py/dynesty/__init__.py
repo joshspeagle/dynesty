c3d852649cb0e0209bcc92d5ea77df71f2b845dd
Sergey E. Koposov <skoposov@ed.ac.uk>
2025-10-04 16:49:32 +0100

    version/packaging changes

--- py/dynesty/__init__.py
+++ py/dynesty/__init__.py
@@ -6,9 +6,15 @@ The main functionality of dynesty is performed by the
 dynesty.NestedSampler and dynesty.DynamicNestedSampler
 classes
 """
+from importlib.metadata import version, PackageNotFoundError
+
 from .dynesty import NestedSampler, DynamicNestedSampler
 from . import bounding
 from . import utils
 from . import pool
 
-from ._version import __version__
+try:
+    __version__ = version("dynesty")
+except PackageNotFoundError:
+    # package is not installed
+    pass
